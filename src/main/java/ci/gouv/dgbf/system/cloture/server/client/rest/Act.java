9b5471bab2b761c28c0bbf229270516a3ddc3632
devlopper <kycdev@gmail.com>
2021-12-15 12:15:19 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/cloture/server/client/rest/Act.java
+++ src/main/java/ci/gouv/dgbf/system/cloture/server/client/rest/Act.java
@@ -9,6 +9,7 @@ import org.cyk.utility.__kernel__.object.AbstractObject;
 import org.cyk.utility.service.client.SpecificServiceGetter;
 
 import ci.gouv.dgbf.system.cloture.server.api.persistence.ActOperationType;
+import ci.gouv.dgbf.system.cloture.server.api.persistence.ActType;
 import ci.gouv.dgbf.system.cloture.server.api.service.ActDto;
 import ci.gouv.dgbf.system.cloture.server.api.service.ActService;
 import lombok.Getter;
@@ -21,9 +22,12 @@ public class Act extends AbstractObject implements Serializable {
 	@JsonbProperty(value = ActDto.JSON_IDENTIFIER) private String identifier;
 	@JsonbProperty(value = ActDto.JSON_CODE) private String code;	
 	@JsonbProperty(value = ActDto.JSON_NAME) private String name;
-	@JsonbProperty(value = ActDto.JSON_OPERATION_TYPE) ActOperationType operationType;
-	@JsonbProperty(value = ActDto.JSON_TRIGGER) String trigger;
-	@JsonbProperty(value = ActDto.JSON_OPERATION_DATE_STRING) String operationDateString;
+	@JsonbProperty(value = ActDto.JSON_TYPE) private ActType type;
+	@JsonbProperty(value = ActDto.JSON_OPERATION_TYPE) private ActOperationType operationType;
+	@JsonbProperty(value = ActDto.JSON_TRIGGER) private String trigger;
+	@JsonbProperty(value = ActDto.JSON_OPERATION_DATE_STRING) private String operationDateString;
+	@JsonbProperty(value = ActDto.JSON_STATUS_STRING) private String statusString;
+	@JsonbProperty(value = ActDto.JSON_LATEST_OPERATION_STRING) private String latestOperationString;
 	
 	@JsonbProperty(value = ActDto.JSON_LOCKED) private Boolean locked;
 	@JsonbProperty(value = ActDto.JSON_NUMBER_OF_LOCKS) private Integer numberOfLocks;
@@ -41,8 +45,11 @@ public class Act extends AbstractObject implements Serializable {
 	public static final String FIELD_IDENTIFIER = "identifier";
 	public static final String FIELD_CODE = "code";
 	public static final String FIELD_NAME = "name";
+	public static final String FIELD_TYPE = "type";
 	public static final String FIELD_OPERATION_TYPE = "operationType";
 	public static final String FIELD_TRIGGER = "trigger";
 	public static final String FIELD_OPERATION_DATE_STRING = "operationDateString";
 	public static final String FIELD_NUMBER_OF_LOCKS_ENABLED = "numberOfLocksEnabled";
+	public static final String FIELD_STATUS_STRING = "statusString";
+	public static final String FIELD_LATEST_OPERATION_STRING = "latestOperationString";
 }
