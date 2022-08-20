2925668ce259ef2a84f76ed6686dd015d94fdc8e
devlopper <kycdev@gmail.com>
2022-08-20 08:08:55 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/cloture/server/client/rest/Act.java
+++ src/main/java/ci/gouv/dgbf/system/cloture/server/client/rest/Act.java
@@ -9,8 +9,6 @@ import org.cyk.utility.__kernel__.DependencyInjection;
 import org.cyk.utility.__kernel__.object.AbstractObject;
 import org.cyk.utility.service.client.SpecificServiceGetter;
 
-import ci.gouv.dgbf.system.cloture.server.api.persistence.ActOperationType;
-import ci.gouv.dgbf.system.cloture.server.api.persistence.ActTypeEnum;
 import ci.gouv.dgbf.system.cloture.server.api.service.ActDto;
 import ci.gouv.dgbf.system.cloture.server.api.service.ActService;
 import lombok.Getter;
@@ -23,15 +21,10 @@ public class Act extends AbstractObject implements Serializable {
 	@JsonbProperty(value = ActDto.JSON_IDENTIFIER) private String identifier;
 	@JsonbProperty(value = ActDto.JSON_CODE) private String code;	
 	@JsonbProperty(value = ActDto.JSON_NAME) private String name;
-	@JsonbProperty(value = ActDto.JSON_TYPE) private ActTypeEnum type;
 	@JsonbProperty(value = ActDto.JSON_TYPE_AS_STRING) private String typeAsString;
-	@JsonbProperty(value = ActDto.JSON_OPERATION_TYPE) private ActOperationType operationType;
-	@JsonbProperty(value = ActDto.JSON_TRIGGER) private String trigger;
-	@JsonbProperty(value = ActDto.JSON_OPERATION_DATE_STRING) private String operationDateString;
-	@JsonbProperty(value = ActDto.JSON_STATUS_STRING) private String statusString;
-	@JsonbProperty(value = ActDto.JSON_LATEST_OPERATION_STRING) private String latestOperationString;
-	
+
 	@JsonbProperty(value = ActDto.JSON_LOCKED) private Boolean locked;
+	@JsonbProperty(value = ActDto.JSON_LOCKED_AS_STRING) private String lockedAsString;
 	@JsonbProperty(value = ActDto.JSON_LOCKED_REASONS) ArrayList<String> lockedReasons;
 	@JsonbProperty(value = ActDto.JSON_NUMBER_OF_LOCKS) private Integer numberOfLocks;
 	@JsonbProperty(value = ActDto.JSON_NUMBER_OF_LOCKS_ENABLED) private Integer numberOfLocksEnabled;
@@ -50,11 +43,7 @@ public class Act extends AbstractObject implements Serializable {
 	public static final String FIELD_NAME = "name";
 	public static final String FIELD_TYPE = "type";
 	public static final String FIELD_TYPE_AS_STRING = "typeAsString";
-	public static final String FIELD_OPERATION_TYPE = "operationType";
-	public static final String FIELD_TRIGGER = "trigger";
-	public static final String FIELD_OPERATION_DATE_STRING = "operationDateString";
+	public static final String FIELD_LOCKED_AS_STRING = "lockedAsString";
 	public static final String FIELD_NUMBER_OF_LOCKS_ENABLED = "numberOfLocksEnabled";
 	public static final String FIELD_LOCKED_REASONS = "lockedReasons";
-	public static final String FIELD_STATUS_STRING = "statusString";
-	public static final String FIELD_LATEST_OPERATION_STRING = "latestOperationString";
 }
